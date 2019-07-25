1c1448fed76078a6b93ceeff65d571a518cae05c
wjddntjr555w <wjddntjr555w@naver.com>
2019-07-26 02:03:48 +0900

    add signup

--- app/src/main/java/com/example/benepicture/MainActivity.kt
+++ app/src/main/java/com/example/benepicture/MainActivity.kt
@@ -3,7 +3,8 @@ package com.example.benepicture
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_login.login_button
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +14,11 @@ class MainActivity : AppCompatActivity() {
 
         login_button.setOnClickListener{
             val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        signup.setOnClickListener{
+            val intent = Intent(this, signupActivity::class.java)
             startActivity(intent)
         }
     }
